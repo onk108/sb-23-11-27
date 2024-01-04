97f732264634111d91c7f4a0bbe39f4cad6a07c7
onk108 <wkdtnstjr88@naver.com>
2024-01-04 19:14:57 +0900

    3강-1

--- src/main/java/com/ll/sb231127/Sb231127Application.java
+++ src/main/java/com/ll/sb231127/Sb231127Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sb231127Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Sb231127Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Sb231127Application.class, args);
+	}
 
 }
